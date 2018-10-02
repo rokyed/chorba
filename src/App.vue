85b34422fab570f9f1d34f68fb55d248f55f7719
Andrei Bazavan <rokyed@gmail.com>
2018-10-01 20:35:14 -0400

    miau

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,64 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+	<el-container>
+	  <el-aside width="200px">Aside</el-aside>
+	  <el-container>
+	    <el-header>Header</el-header>
+	    <el-main>
+			<router-view />
+		</el-main>
+	    <el-footer>Footer</el-footer>
+	  </el-container>
+	</el-container>
 </template>
 
 <script>
 export default {
-  name: 'App'
+	name: 'App'
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+
+.el-header,
+.el-footer {
+	background-color: #B3C0D1;
+	color: #333;
+	text-align: center;
+	line-height: 60px;
+}
+
+.el-aside {
+	background-color: #D3DCE6;
+	color: #333;
+	text-align: center;
+	line-height: 200px;
+}
+
+.el-main {
+	background-color: #E9EEF3;
+	color: #333;
+	text-align: center;
+	line-height: 160px;
+}
+
+body>.el-container {
+	margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+	line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+	line-height: 320px;
 }
 </style>
